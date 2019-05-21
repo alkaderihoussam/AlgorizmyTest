05c4f915355288d5833022741f3bc50d8f42d41f
D-SF41EV <Mohamed-Larbi.Chouaiar@ing.com>
2019-05-21 15:02:26 +0200

    final version

--- src/app/animals-service.ts
+++ src/app/animals-service.ts
@@ -8,17 +8,8 @@ export class AnimalsService {
 
   constructor(private httpClient: HttpClient) {}
 
-  // getAnimalsPerFamily(family: string) {
-  //   return this.httpClient.get<any>(' http://localhost:3000/animals')
-  //     .pipe((filter((res: Animal) => res.family === family)));
-  // }
-
   getAnimals() {
     return this.httpClient.get<any>(' http://localhost:3000/animals')
       .pipe((map((res: Animal[]) => res)));
-    // return this.httpClient.get<any>(' http://localhost:3000/animals')
-    //   .toPromise()
-    //   .then(res => <Animal[]> res)
-    //   .then(data => { return data});
   }
 }
